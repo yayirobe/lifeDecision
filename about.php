eda0b136c62042332b9e43f49e421451e7955bd6
yayirobe <yaser.asmat@gmail.com>
2019-09-29 21:26:47 -0300

    Estructura Proyecto
    Primera subida de archivos

---
+++ about.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Quienes somos?</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="preloader">
+      <div class="preloader-body">
+        <div class="cssload-container">
+          <div class="cssload-speeding-wheel"></div>
+        </div>
+        <p>Cargando...</p>
+      </div>
+    </div>
+    <div class="page">
+      <!--Header-->
+      <header class="section page-header">
+        <!--RD Navbar-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-static" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-md-stick-up-offset="185px" data-lg-stick-up-offset="185px" data-xl-stick-up-offset="185px" data-xxl-stick-up-offset="185px" data-md-stick-up="true" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+            <div class="rd-navbar-section rd-navbar-static-collapse">
+              <div class="rd-navbar-container">
+                <div class="rd-navbar-panel">
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav"><span></span></button>
+                  <button class="rd-navbar-info-toggle" data-rd-navbar-toggle=".rd-navbar-info"><span></span></button>
+                </div>
+              </div>
+            </div>
+            <div class="rd-navbar-section">
+              <div class="rd-navbar-container">
+                <div class="rd-navbar-logo">
+                  <a href="./">
+                    <img class="logo-small" src="images/LogoLife.png"/>
+                    <img class="logo-default" src="images/LogoLife.png"/>
+                  </a>
+                </div>
+                  <ul class="rd-navbar-nav">
+                    <li class="rd-nav-item active"><a class="rd-nav-link" href="./about">¿Qui&eacute;nes somos?</a></li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Agenda</a></li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Amigos Life</a></li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#">Donaciones</a></li>
+                  </ul>
+                  <div class="rd-navbar-icons">
+                    <i class="fab fa-facebook-square"></i>
+                    <i class="fab fa-twitter-square"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-youtube-square"></i>
+                  </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <!--About Us-->
+      <section class="section-sm">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-lg-3 col-xl-4">
+              <h2 class="text-uppercase">Quienes <span class="font-weight-light">Somos</span></h2><img src="images/about/Pastores.jpeg" alt="" width="370" height="137"/>
+              <p class="text-primary font-weight-semibold">Nuestros Pastores</p>
+              <p class="text-justify">Pastores de jóvenes y fundadores del Ministerio Life Decision con sede central en la ciudad de Rosario. Ejercen un Ministerio enfocado en las generaciones emergentes. Casados desde el año 2013, papás de tres niñas: Holly y las gemelas Annie y Joyce.</p>
+            </div>
+            <div class="col-lg-5 col-xl-4 text-justify">
+              <h2 class="text-uppercase">Nuestra <span class="font-weight-light">Historia</span></h2>
+              Convocados por el Espíritu Santo, nace esta iglesia en el centro de la Ciudad de Rosario, Provincia de Santa Fe, Argentina, enfocados en el trabajo con la Generación Emergente.
+              El 26 de Mayo de 2013, se funda con el nombre de Ministerio Tabernáculo de Rosario bajo la guía del Espíritu Santo con un fuerte llamado hacia la nueva Generación; “…Hay mucho pueblo en Rosario por ser alcanzado…” fueron las palabras específicas que dieron comienzo a este gran desafío de Fe. 
+              Desde la primera convocatoria llegaron jóvenes de diferentes provincias, denominaciones y culturas pero con un mismo sentir, Ser Parte de lo que Dios está haciendo en nuestra Nación como así también en el Mundo entero. Ellos fueron la primicia de todo lo que vendría.
+              Cimentados en el ejercicio de los cinco Ministerios como lo expresa Efesios 4:11-12 
+              <ul class="list-marked">
+                <li><a class="list-marked-item" href="#">11“…y El mismo constituyó a unos, apóstoles; a otros, profetas; a otros, evangelistas; a otros, pastores y maestros</a></li>
+                <li><a class="list-marked-item" href="#">12 a fin de perfeccionar a los santos para la obra del ministerio, para la edificación del cuerpo de Cristo…”</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4 col-xl-4 text-justify">
+              <h2 class="text-uppercase">Objetivos <span class="font-weight-light">Estratégicos</span></h2>
+                <div class="time-point">
+                  <time class="time-point-date">Misión</time>
+                  <div class="time-point-description">Predicar el evangelio completo de Jesucristo. Sacar a las personas del territorio de muerte en el que están parados y posicionarlos en el territorio de vida, esto se logra con intercesión profética y guerra espiritual, ministraciones sistemáticas y enseñanza sobre el evangelio completo de Jesucristo y decisiones.</div>
+                </div>
+                <div class="time-point">
+                  <time class="time-point-date">Visión</time>
+                  <div class="time-point-description">Formar y entrenar a esta generación en cada una de las áreas de la vida, tanto espiritual, ministerial, profesional como personal.</div>
+                </div>
+                <div class="time-point">
+                  <time class="time-point-date">Valores</time>
+                  <div class="time-point-description">Lugar donde definimos nuestro destino espiritual a través de decisiones impulsadas por Su Espíritu, donde bebemos y disfrutamos de la abundancia de Su Presencia.</div>
+                </div>
+                <div class="time-point">
+                  <time class="time-point-date">Objetivo</time>
+                  <div class="time-point-description">Mantenerse enfocados en el trabajo con la Generación Emergente.</div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--Overview-->
+      <section class="section-sm">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-6 col-lg-4">
+              <h2 class="text-uppercase">Advantages</h2>
+              <ul class="list-ordered">
+                <li class="list-ordered-item">
+                    Diseños
+                    <ul>
+                        <li>Niveles de autoridad: Familia, hombre, mujer, joven, anciano, economía, administración, salud, adoración, entre otros.</li>
+                    </ul>
+                </li>
+                <li class="list-ordered-item">
+                  Ministerios y espacios ministeriales
+                  <ul>
+                      <li>Ejercicio de dones ministeriales y ministerios. Efesios 4:11-16, 1 Corintios 12:4-11</li>
+                  </ul>
+                </li>
+                <li class="list-ordered-item">
+                    Generacional
+                    <ul>
+                        <li>Legado recibido y legado que dejaremos. Deuteronomio 7:9, Mateo 22:32</li>
+                    </ul>
+                </li>
+                <li class="list-ordered-item">
+                  Vida del Espíritu y revelación
+                    <ul>
+                        <li>Práctica diaria de Su Presencia. Apocalipsis 19:10</li>
+                    </ul>
+                </li>
+            </ul>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <h2 class="text-uppercase">Why Us</h2>
+              <p class="text-primary font-weight-semibold">8 Reasons to Choose Our Company</p>
+              <p>Curabitur eu lorem ac lacus laoreet auctor. Fusce vitae orci nec velit ornare rhoncus ut tempus est. Mauris eu augue lorem. Suspendisse sit amet vehi cula nisl, nec faucibus nisl. Proin ac fermentum orci, non semper metus. Nulla nulla tellus</p>
+              <ul class="list-marked">
+                <li><a class="list-marked-item" href="#">Fusce itae orci nec velit ornare rhon</a></li>
+                <li><a class="list-marked-item" href="#">Ecus ut tempus estauris eu augue lorem.</a></li>
+                <li><a class="list-marked-item" href="#">Suspendisse sit amet vehicula</a></li>
+                <li><a class="list-marked-item" href="#">Anisl, nec faucibus nislroin ac fermentum</a></li>
+                <li><a class="list-marked-item" href="#">Horci, non semper metusulla nulla</a></li>
+                <li><a class="list-marked-item" href="#">Dellus, tincidunt vel eros gravida, cursu</a></li>
+                <li><a class="list-marked-item" href="#">Nullam ac magna nisi. Integer</a></li>
+                <li><a class="list-marked-item" href="#">Dictum sagittis vulputate ulla a purus</a></li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <h2 class="text-uppercase">Testimonial</h2>
+                    <blockquote class="testimonial">
+                      <div class="testimonial-img"><img src="images/persons/person-05-170x168.jpg" alt="" width="170" height="168"/>
+                      </div>
+                      <div class="testimonial-inner">
+                        <q class="testimonial-quote">Curabitur eu lorem ac lacus laoreet auctor. Fusce vitae orci nec velit ornare rhoncus ut tem pus est. Mauris eu aug ue lorem. Suspendisse sit amet vehi cul</q>
+                        <cite class="testimonial-author">Edna Barton, <br class="d-none d-xl-block">client</cite>
+                      </div>
+                    </blockquote>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="form-output snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
