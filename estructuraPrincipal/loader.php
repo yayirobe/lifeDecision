fabb85160ff208c9f8c0148cf0fe61693a380292
Yaser <yaser.asmat@gmail.com>
2019-10-14 17:07:10 -0300

    Master Page

---
+++ estructuraPrincipal/loader.php
@@ -0,0 +1,8 @@
+<div id="loader" class="preloader">
+  <div class="preloader-body">
+    <div class="cssload-container">
+      <div class="cssload-speeding-wheel"></div>
+    </div>
+    <p>Cargando...</p>
+  </div>
+</div>
